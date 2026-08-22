086de5e1eac3b135074cb333bc150abbf605ff63
Prasad Bange <prasadbange2@gmail.com>
2026-08-22 20:05:18 +0530

    Firestore rules updated

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,6 +1,7 @@
 export const ALLOWED_ADMIN_EMAILS = [
   'tarkadesignstudio@gmail.com',
-  'ssd.motwani@gmail.com'
+  'ssd.motwani@gmail.com',
+  'ssdgiftcollection7@gmail.com'
 ];
 
 export const isAdmin = (user: { email?: string | null } | null) => {
